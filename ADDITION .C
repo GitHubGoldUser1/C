2951d6742571f0415fd015458ac4ef92212b9b78
Gold Bull <165162257+GitHubGoldUser1@users.noreply.github.com>
2024-04-06 18:04:48 +0530

    ADDITION .C

---
+++ ADDITION .C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+//Code to Add two Numbers
+int x;
+int y;
+printf("Enter any number :  \n");
+ scanf("%d",&x);
+printf("Enter any number :  \n");
+ scanf("%d",&y);
+int z=x+y;
+printf("%d",x);
+printf(" + %d",y);
+printf(" = %d",z);
+return 0;
+}
