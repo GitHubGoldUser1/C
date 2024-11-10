e905d1c0f5cff14aaa530c7193d116030eda194e
GOLD BULL <165162257+GitHubGoldUser1@users.noreply.github.com>
2024-11-10 17:17:04 +0530

    Myself_Essay_generator.C
    Simple essay generator ...

---
+++ Myself_Essay_generator.C
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+char name[100];
+int age;
+printf("Enter your name ");
+scanf("%s",name);
+printf("Enter your age ");
+scanf("%d",&age);
+printf("\n\nMy Self Essay in English 150 Words\n\n\n");
+printf("My name is %s and my age is %d \n",name,age);
+printf("I am someone who believes in growing\n"); 
+printf("and learning every day. I think it’s\n"); 
+printf("important to learn from each experience\n"); 
+printf("and always try to improve. I value\n"); 
+printf("kindness, honesty, and hard work,\n");
+printf("and I try to treat others with respect.\n"); 
+printf("I am curious and enjoy exploring new\n");
+printf("ideas, which helps me understand\n");
+printf("different view points.\n\n");
+
+printf("In my life, I focus on both my studies\n"); 
+printf("and personal growth. I enjoy reading,\n"); 
+printf("spending time with family and friends,\n"); 
+printf("and doing creative activities that\n"); 
+printf("inspire me. I believe that true\n"); 
+printf("success is not only about achieving\n"); 
+printf("personal goals but also about helping\n"); 
+printf("and supporting others.\n");
+
+printf("As I look to the future, I want to\n");
+printf("achieve my dreams, but I also want\n");
+printf("to make a positive difference in the\n"); 
+printf("world. I believe everyone can contribute\n"); 
+printf("to making the world a better place, and\n"); 
+printf("I hope to do my part.\n");
+char task;
+printf("\n\n\nPress Enter to Exit");
+scanf("%s",task);
+return 0;
+}
